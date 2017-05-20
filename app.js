731610d263916e6d888baa68c19f03ed07d54f36
lalithalnt <lalitha445@gmail.com>
2017-05-20 15:36:31 +0530

    update all the files

--- app.js
+++ app.js
@@ -1,13 +1,58 @@
 var app=angular.module("myApp" , []);
 app.controller("myController" , ["$scope" , function($scope){
-$scope.message="welcome to hyderabad"
+$scope.message="welcome to hyderabad";
+$scope.companyName="techminds"
+$scope.arrayUsers=
+[
+{
+     name : "puspa biswal",
+     address : "madhapur , hyderabad"
+ } ,
+
+ {
+     name : "pooja biswal",
+     address : "madhapur , hyderabad"
+ },
+
+
+ {
+     name : "liza biswal",
+     address : "madhapur , hyderabad"
+ }
+
+]
+
+
+
+
+
+$scope.userDetails ={
+     name : "puspa biswal",
+     address : "madhapur , hyderabad"
+ }
+
+$scope.changeCompanyName = function()
+{
+	$scope.companyName= "techmind solutions";
+}
+
 }])
 
 app.directive("myCustomerDetailsDirective" , function(){
 return{
 restrict : 'EACM' ,
-template : "<h1>this is my custom directives</h1>" ,
-replace :true
+templateUrl : 'customerdetails.html' ,
+ replace :true ,
+            scope : 
+            {
+             companyName : '@' ,
+             userDetails : '=' ,
+             changeCompanyName : '&' ,
+            arrayUsers : '='
+
+              }
+
+
 }
 
-})
+} )
